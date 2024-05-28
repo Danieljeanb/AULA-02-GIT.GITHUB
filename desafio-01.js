8d6f34aa227eb9894fe840149169303c8b0dd926
Danieljeanbe <danieljean.o.bastos@gmail.com>
2024-05-28 20:21:46 -0300

    Primeiro dessafio

---
+++ desafio-01.js
@@ -0,0 +1,15 @@
+// Faça duas variveis numericas e descubra qual é a maior
+
+let a = 6
+let b = 17
+
+if(a > b){
+  console.log('A maior que B')
+}
+else if (a < b){
+   console.log('B é maior que A')
+}
+
+else {
+    console.log('São iguais')
+}
